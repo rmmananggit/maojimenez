5e5f45432a5964d6c66ce3ef2176861d5785d0b8
franzcarl13 <franzcarl13@yahoo.com>
2023-02-08 00:14:10 +0800

    removed unused css

--- login/index.php
+++ login/index.php
@@ -24,7 +24,6 @@
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="fonts/icomoon/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
   </head>
 
   <?php
