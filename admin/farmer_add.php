78052a2be8307e9ed09c41a104c2361c9d99ef10
Francis Carlo A. Manlangit <francismanlangit13@yahoo.com>
2023-04-05 11:59:08 +0800

    fix farmer_add.php

--- admin/farmer_add.php
+++ admin/farmer_add.php
@@ -39,21 +39,6 @@
                                 <input placeholder="Enter Middle Name" type="text" name="mname" class="form-control">
                             </div>
 
-                    
- 
-                    <form action="code.php" method="POST" autocomplete="off" enctype="multipart/form-data"> 
-                    <div class="row"> 
-                                <div class="col-md-4 mb-3">
-                                    <label for="" class="required">Last Name</label>
-                                    <input required placeholder="Enter First Name" type="text" name="lname" class="form-control">
-                                </div> 
-                            
-
-                                <div class="col-md-4 mb-3">
-                                    <label for="">Middle Name</label>
-                                    <input placeholder="Enter Middle Name" type="text" name="mname" class="form-control">
-                                </div>
-
                             <div class="col-md-3 mb-3">
                                 <label for="" class="required">First Name</label>
                                 <input required placeholder="Enter First Name" type="text" name="fname" class="form-control">
@@ -176,73 +161,17 @@
 
                             <div class="col-md-3 mb-3">
                                 <label for="" class="required">Civil Status</label>
-                                    <select name="civilstatus" required class="form-control">
-                                        <option value="None" selected="true" disabled="disabled">--Select Civil Status--</option>
-                                        <option value="Single">Single</option>
-                                        <option value="Married">Married</option>
-                                        <option value="Widowed">Widowed</option>
-                                        <option value="Separated">Separated</option>
-                                    </select>
-                                </div>
-                            </div>
-
-                            <div class="col-md-3 mb-3">
-                                <label for="" class="required">Gender</label>
-                                <br>
-                                <input required class="ml-2" type="radio" name="gender" value="Male"> Male
-                                <input required class="ml-2"  type="radio" name="gender" value="Female"> Female
-                            </div>
-
-                            <div class="col-md-9 mb-3">
-                                <label for="" class="required">Email Address</label>
-                                <input required placeholder="Enter Email Address" type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" class="form-control">
+                                <select name="civilstatus" required class="form-control">
+                                    <option value="" selected="true" disabled="disabled">Select Civil Status</option>
+                                    <option value="Single">Single</option>
+                                    <option value="Married">Married</option>
+                                    <option value="Widowed">Widowed</option>
+                                    <option value="Separated">Separated</option>
+                                </select>
                             </div>
 
                             <div class="col-md-12 mb-3">
                                 <hr>
-                            </div>
-
-                            <div class="col-md-4 mb-3">   
-                                <label for="" class="required">Purok</label>
-                                <input required placeholder="Enter Purok No." type="text" name="purok" class="form-control">
-                            </div>
-
-                            <div class="col-md-4 mb-3">
-                                <label for="" class="required">Street</label>
-                                <input required placeholder="Enter Street" type="text" name="street" class="form-control">
-                            </div>
-
-                            <div class="col-md-4 mb-3">
-                                <div class="form-group">
-                                    <label for="address" class="required">Municipality</label>
-                                    <select class="form-control" name="barangay" id="barangay" required>
-                                        <option value="" selected="true" disabled="disabled">Select Municipality</option>
-                                        <option value="Adorable">Adorable</option>    
-                                        <option value="Butuay">Butuay</option> 
-                                        <option value="Carmen">Carmen</option> 
-                                        <option value="Corrales">Corrales</option> 
-                                        <option value="Dicoloc">Dicoloc</option> 
-                                        <option value="Gata">Gata</option> 
-                                        <option value="Guintomoyan">Guintomoyan</option> 
-                                        <option value="Malibacsan">Malibacsan</option> 
-                                        <option value="Macabayao">Macabayao</option> 
-                                        <option value="Matugas Alto">Matugas Alto</option> 
-                                        <option value="Matugas Bajo">Matugas Bajo</option> 
-                                        <option value="Mialem">Mialem</option> 
-                                        <option value="Naga">Naga</option> 
-                                        <option value="Palilan">Palilan</option> 
-                                        <option value="Nacional">Nacional</option> 
-                                        <option value="Rizal">Rizal</option>
-                                        <option value="San Isidro">San Isidro</option> 
-                                        <option value="Santa Cruz">Santa Cruz</option>
-                                        <option value="Sibaroc">Sibaroc</option>
-                                        <option value="Sinara Alto">Sinara Alto</option>
-                                        <option value="Sinara Bajo">Sinara Bajo</option>
-                                        <option value="Seti">Seti</option>
-                                        <option value="Tabo-o">Tabo-o</option>
-                                        <option value="Taraka">Taraka</option>
-                                    </select>
-                                </div>
                             </div>
 
                             <div class="col-md-4 mb-3">
@@ -270,32 +199,9 @@
                             <input required class="ml-2" type="radio" name="ig" value="No"> No
                             </div>
 
-                                <div class="col-md-4 mb-3">
-                                <label for="" class="required">Member of an <strong>Indigenous Group</strong>?</label>
-                                <br>
-                                <input required class="ml-2" type="radio" name="ig" value="Yes"> Yes
-                                <input required class="ml-2" type="radio" name="ig" value="No"> No
-                                </div>
-
-                            <div class="col-md-4 mb-3">
-                            <label for="" class="required">With <strong>Government ID</strong>?</label>
-                            <br>
-                            <input required class="ml-2" type="radio" name="govid" value="Yes"> Yes
-                            <input required class="ml-2" type="radio" name="govid" value="No"> No
-                            </div>
-
-                            <div class="col-md-4 mb-3">
-                            <label for="" class="required">Member of an <strong>Indigenous Group</strong>?</label>
-                            <br>
-                            <input required class="ml-2" type="radio" name="ig" value="Yes"> Yes
-                            <input required class="ml-2" type="radio" name="ig" value="No"> No
-                            </div>
-
-                            <div class="col-md-4 mb-3">
-                            <label for="" class="required">Member of any <strong>Farmers Association/Cooperative</strong>?</label>
-                            <br>
-                            <input required class="ml-2" type="radio" name="fac" value="Yes"> Yes
-                            <input required class="ml-2"  type="radio" name="fac" value="No"> No
+                            <div class="col-md-8 mb-3">
+                                <label for="">If yes, specify:</label>
+                                <input  placeholder="" type="text" name="igyes" class="form-control" value="<?php echo (isset($_POST['ig']) && $_POST['ig'] == 'No') ? 'N/A' : ''; ?>">
                             </div>
 
                             <div class="col-md-4 mb-3">
@@ -338,131 +244,6 @@
                                 <input required class="ml-4" type="radio" name="livelihood" value="Farmer" id="option1" onclick="showDiv('div1')"> Farmer
                                 <input required  class="ml-4" type="radio" name="livelihood" value="Farmworker" id="option2" onclick="showDiv('div2')"> Farmworker/Laborer
                                 <input required  class="ml-4" type="radio" name="livelihood" value="Agri Youth" id="option3" onclick="showDiv('div3')"> Agri Youth
-                            </div>
-
-                            <div class="col-md-12 mb-3"  id="div1" style="display: none;">
-                                <h5 class="text-center mt-4"><b>FOR FARMER</b></h5>
-                                <h6 class="text-center mt-4"><u>Type of Farming Activity</u></strong></h6>
-                            
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="Rice" name="rice">
-                                    <label class="form-check-label" for="rice">Rice</label>
-                                </div>
-
-                                <div class="form-check mt-2">
-                                    <input class="form-check-input" type="checkbox" value="Corn" name="corn">
-                                    <label class="form-check-label" for="corn">Corn</label>
-                                </div>
-
-                                <label fowr="">Other Crops Specify:</label>
-                                <input placeholder="" type="text" name="other_crops_specify" class="form-control">
-
-                                <div class="form-check mt-2">
-                                    <input class="form-check-input" type="checkbox" value="Livestock" name="livestock">
-                                    <label class="form-check-label" for="livestock">Livestock</label>
-                                </div>
-
-                                <label for="">Specify:</label>
-                                <input required placeholder="" type="text" name="livestock_specify" class="form-control">
-
-                                <div class="form-check mt-2">
-                                    <input class="form-check-input" type="checkbox" value="Poultry" name="poultry">
-                                    <label class="form-check-label" for="poultry">Poultry</label>
-                                </div>
-                                
-                                <label for="">Specify:</label>
-                                <input required placeholder="" type="text" name="poultry_specify" class="form-control">
-                            </div>
-
-                            <div class="col-md-12 mb-3" id="div2" style="display: none;">
-                                <h5 class="text-center mt-4"><b>FOR FARMWORKER</b></h5>
-                                <h6 class="text-center mt-4"><u>Kind of Work</u></strong></h6>
-
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="Owner" name="owner">
-                                    <label class="form-check-label" for="owner">Owner</label>
-                                </div>
-
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="Land Preparation" name="land">
-                                    <label class="form-check-label" for="land">Land Preparation</label>
-                                </div>
-
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="Cultivation" name="cultivation">
-                                    <label class="form-check-label" for="cultivation">Cultivation</label>
-                                </div>
-
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="Planting" name="planting">
-                                    <label class="form-check-label" for="planting">Planting</label>
-                                </div>
-
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="Harvesting" name="harvesting">
-                                    <label class="form-check-label" for="Harvesting">Harvesting</label>
-                                </div>
-                                    
-                                <label for="">Others, Please Specify:</label>
-                                <input placeholder="" type="text" name="othersfarmworker" class="form-control">
-                            </div>
-
-                            <div class="col-md-12 mb-3" id="div3" style="display: none;">
-                                <h5 class="text-center mt-4"><b>FOR AGRI YOUTH</b></h5>
-                                <h6 class="text-center mt-4"><u>Type of Involvement</u></strong></h6>
-                                <p class="text-info">For the purpose of trainings, financial assistance, and other programs catered to the youth with involvement to any agriculture activity.</p>
-
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="Part of a farming household" name="part_of_farming">
-                                    <label class="form-check-label" for="Part of a farming household">Part of a farming household</label>
-                                </div>
-
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="Attending/Attended formal agri-fishery related course" name="attending_formal">
-                                    <label class="form-check-label" for="Attending/Attended formal agri-fishery related course">Attending/Attended formal agri-fishery related course</label>
-                                </div>
-
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="Attending/Attended non-formal agri-fishery related course" name="attending_nonformal">
-                                    <label class="form-check-label" for="Attending/Attended non-formal agri-fishery related course">Attending/Attended non-formal agri-fishery related course</label>
-                                </div>
-
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="Participated in any agricultural activity/program" name="participated">
-                                    <label class="form-check-label" for="Participated in any agricultural activity/program">Participated in any agricultural activity/program</label>
-                                </div>
-
-                                <label for="">Others, Please Specify:</label>
-                                <input placeholder="" type="text" name="other_agri_youth_specify" class="form-control">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <br>
-                <div class="card mt-1">
-                    <div class="card-header">
-                        <h5>PART III: FARM DOCUMENT</h5>
-                    </div>
-                    <div class="card-body"> 
-                        <div class="row">
-                            <div class="col-md-6 mb-3 ml-4">                             
-                                <label for="profilepicture" class="required">Upload 2x2 Picture </label> <br>
-                                <input type="file" name="profilepicture" accept=".jpg, .jpeg, .png" value="" required>
-                            </div>
-
-                            <div class="col-md-3 mb-3 qrcode"> 
-                                <label class="required">
-                                    SCAN QR CODE HERE
-                                    <input required type="text" name="qrcode_text" id="qrscan" readonyy="" style="opacity:1%; margin:-6.2rem;">
-                                </label>
-                                <video id="preview" width="100%"></video>
-                            </div>
-
-                            <div class="col-md-3 mb-3 succqrcode" style="display:none"> 
-                                <div class="alert alert-success" role="alert">
-                                    QR Scan Success!
-                                </div>
                             </div>
 
                             <div class="col-md-12 mb-3"  id="div1" style="display: none;">
