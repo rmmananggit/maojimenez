59de3c6730ebb6dd75702d78923ddea5630293fa
Rochell Mark Mananggit <107897402+rmmananggit@users.noreply.github.com>
2023-04-12 14:01:09 +0800

    sms

---
+++ admin/sms.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$ch = curl_init();
+$parameters = array(
+    'apikey' => 'cda2b7bcdab4a6ab448b7618c4721f59', //Your API KEY
+    'number' => '09383926318',
+    'message' => 'Announcement! All farmers mag meeting ta karong adlawa.',
+    'sendername' => 'CabTom'
+);
+curl_setopt( $ch, CURLOPT_URL,'https://semaphore.co/api/v4/messages' );
+curl_setopt( $ch, CURLOPT_POST, 1 );
+
+//Send the parameters set above with the request
+curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $parameters ) );
+
+// Receive response from server
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+$output = curl_exec( $ch );
+curl_close ($ch);
+
+//Show the server response
+echo $output;
+
+?>
