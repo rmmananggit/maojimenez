fe5c9ca764c68dcf829739ced97a42ad2d8c68b6
Rochell Mark Mananggit <107897402+rmmananggit@users.noreply.github.com>
2023-04-12 15:44:44 +0800

    fixes sms

--- admin/sms.php
+++ admin/sms.php
@@ -1,11 +1,16 @@
 <?php
 
+$string = <<<EOD
+Announcement! This is from the MAO Jimenez Office. 
+When: Meeting 
+Where: Jimenez Gymnasium
+EOD;
 
 $ch = curl_init();
 $parameters = array(
     'apikey' => 'cda2b7bcdab4a6ab448b7618c4721f59', //Your API KEY
-    'number' => '09383926318',
-    'message' => 'Announcement! All farmers mag meeting ta karong adlawa.',
+    'number' => '09457664949',
+    'message' => $string ,
     'sendername' => 'CabTom'
 );
 curl_setopt( $ch, CURLOPT_URL,'https://semaphore.co/api/v4/messages' );
