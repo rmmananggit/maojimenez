f9805131f10c2e5a7cc14820493b161ba4492ffb
Rochell Mark Mananggit <107897402+rmmananggit@users.noreply.github.com>
2023-05-22 15:09:55 +0800

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -9,7 +9,7 @@
     // This is offline hosting configuration.
     if(!defined('is_mobile')) define('is_mobile', is_numeric(strpos(strtolower($_SERVER["HTTP_USER_AGENT"]), "mobile")));
     if(!defined('date')) define('date', date("Y-m-d h:i:s A"));
-    if(!defined('base_url')) define('base_url','http://localhost/municipalagricultureoffice/');
+    if(!defined('base_url')) define('base_url','http://localhost/maojimenez/');
     if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
     if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
     if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
